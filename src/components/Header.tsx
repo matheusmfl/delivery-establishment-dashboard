61c8bb829654c55c6edaa0b581d188b9704fab27
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-26 18:35:03 -0300

    Create header components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,60 @@
+import { Menu } from '@mui/icons-material'
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from '@mui/material'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
 export function Header() {
-  return <header></header>
+  const pageTitle = 'Painel B7Delivery'
+  const router = useRouter()
+
+  function handleLogout() {
+    router.push('/login')
+  }
+  function handleDrawerToggle() {}
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            edge="start"
+            sx={{ display: { sm: 'none' } }}
+            onClick={handleDrawerToggle}
+          >
+            <Menu />
+          </IconButton>
+          <Typography
+            component="div"
+            variant="h6"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            <Link href="/" style={{ color: '#fff', textDecoration: 'none' }}>
+              {pageTitle}
+            </Link>
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'flex' }, gap: '16px' }}>
+            <Link href="/pedidos" style={{ textDecoration: 'none' }}>
+              <Button sx={{ color: '#fff' }}> Pedidos</Button>
+            </Link>
+            <Link href="/produtos" style={{ textDecoration: 'none' }}>
+              <Button sx={{ color: '#fff' }}> Produtos</Button>
+            </Link>
+            <Link href="/categorias" style={{ textDecoration: 'none' }}>
+              <Button sx={{ color: '#fff' }}> Categorias</Button>
+            </Link>
+            <Button onClick={handleLogout} sx={{ color: '#fff' }}>
+              Sair
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
 }
